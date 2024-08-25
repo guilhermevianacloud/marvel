e88f98c0c8633c42f558a08a5b2f37ab0013adc9
guilhermevianacloud <guilhermeviana.cloud@gmail.com>
2024-08-25 01:49:06 -0300

    estilizção da página

--- src/App.js
+++ src/App.js
@@ -1,56 +1,190 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const apiKey = '61f674e4830b5c7eef216aa8801e6d15';
-const time = '1724527096';
-const hash = 'd87eaf0608cceaf972e69ad9220b2f5a';
+// Constantes para autenticação na API do Marvel
+const apiKey = "61f674e4830b5c7eef216aa8801e6d15";
+const time = "1724527096";
+const hash = "d87eaf0608cceaf972e69ad9220b2f5a";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [isChecked, setIsChecked] = useState(false);
+    const [favoriteCount, setFavoriteCount] = useState(0);
 
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
+    // Caminhos das imagens de coração preenchido e vazio
+    const heartEmpty = "assets/icones/heart/Path Copy 2.png";
+    const heartFilled = "assets/icones/heart/Path.png";
 
-  const fetchCharacters = () => {
-    fetch(`https://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${apiKey}&hash=${hash}&limit=20`)
-      .then((response) => response.json())
-      .then((jsonParsed) => {
-        setCharacters(jsonParsed.data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
-  };
+    useEffect(() => {
+        fetchCharacters();
+    }, []);
 
-  return (
-    <div>
-      {loading ? (
-        <div className="loading-container">
-          <div className="loading-text">Carregando informações dos personagens</div>
-          <div className="loading-dots">
-            <span className="dot-1">.</span>
-            <span className="dot-2">.</span>
-            <span className="dot-3">.</span>
-          </div>
-        </div>
-      ) : (
-        <div id="herois">
-          {characters.map((character) => (
-            <div key={character.id} className="personagem">
-              <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt={character.name} />
-              <div>
-                <h3>{character.name}</h3>
-              </div>
+    const fetchCharacters = () => {
+        fetch(
+            `https://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${apiKey}&hash=${hash}&limit=20`
+        )
+            .then((response) => response.json())
+            .then((jsonParsed) => {
+                const charactersData = jsonParsed.data.results;
+                // Inicializa a lista de personagens
+                const updatedCharacters = charactersData.map((char) => ({
+                    ...char,
+                    isFavorite: false, // Inicializa como não favorito
+                }));
+                setCharacters(updatedCharacters);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            });
+    };
+
+    const handleToggle = () => {
+        setIsChecked(!isChecked);
+    };
+
+    const toggleFavorite = (id) => {
+        setCharacters((prevCharacters) => {
+            const characterIndex = prevCharacters.findIndex(
+                (char) => char.id === id
+            );
+            if (characterIndex === -1) return prevCharacters;
+
+            const newCharacters = [...prevCharacters];
+            const character = newCharacters[characterIndex];
+            const newFavoriteStatus = !character.isFavorite;
+
+            // Checa se o novo status é favorito e o número total de favoritos
+            if (newFavoriteStatus && favoriteCount >= 5) {
+                console.log("Limite de favoritos atingido.");
+                return prevCharacters;
+            }
+
+            if (newFavoriteStatus) {
+                setFavoriteCount(favoriteCount + 1);
+            } else {
+                setFavoriteCount(favoriteCount - 1);
+            }
+
+            // Atualiza o estado do personagem
+            newCharacters[characterIndex] = {
+                ...character,
+                isFavorite: newFavoriteStatus,
+            };
+            return newCharacters;
+        });
+    };
+
+    // Filtra os personagens com base no estado do toggle
+    const displayedCharacters = isChecked
+        ? characters.filter((char) => char.isFavorite)
+        : characters;
+
+    return (
+        <div className="app">
+            <header className="header">
+                <img
+                    src="assets/logo/Group@2x.png"
+                    alt="Marvel Logo"
+                    className="logo"
+                />
+                <div className="header-text">
+                    <h1>EXPLORE O UNIVERSO</h1>
+                    <p>
+                        Mergulhe no domínio deslumbrante de todos os personagens
+                        clássicos que você ama - e aqueles que você descobrirá
+                        em breve!
+                    </p>
+                </div>
+            </header>
+
+            <div className="search-section">
+                <div className="search-bar">
+                    <img
+                        src="assets/busca/Lupa/Shape.png"
+                        alt="Search Icon"
+                        className="search-icon"
+                    />
+                    <input type="text" placeholder="Procure por heróis" />
+                </div>
+                <div className="controls">
+                    <button className="sort-button">
+                        <img
+                            src="assets/icones/heroi/noun_Superhero_2227044.png"
+                            alt="Hero Icon"
+                        />
+                        Ordenar por nome - A/Z
+                    </button>
+
+                    <div className="toggle-button" onClick={handleToggle}>
+                        <div
+                            className={`toggle-icon ${
+                                isChecked ? "checked" : ""
+                            }`}
+                        ></div>
+                    </div>
+
+                    <label htmlFor="toggle" className="favorites-toggle">
+                        <input
+                            type="checkbox"
+                            checked={isChecked}
+                            onChange={handleToggle}
+                        />
+                        <img
+                            src="assets/icones/heart/Path.png"
+                            alt="Favorites Icon"
+                        />
+                        Somente favoritos
+                    </label>
+                </div>
             </div>
-          ))}
+
+            <div className="heroes-list">
+                {loading ? (
+                    <div className="loading-container">
+                        <div className="loading-text">
+                            Carregando informações dos personagens
+                        </div>
+                        <div className="loading-dots">
+                            <span className="dot-1">.</span>
+                            <span className="dot-2">.</span>
+                            <span className="dot-3">.</span>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="heroes-grid">
+                        {displayedCharacters.map((character) => (
+                            <div key={character.id} className="personagem">
+                                <img
+                                    src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                                    alt={character.name}
+                                    className="personagemImg"
+                                />
+                                <div className="character-info">
+                                    <h3>{character.name}</h3>
+                                    <img
+                                        src={
+                                            character.isFavorite
+                                                ? heartFilled // Se o personagem é favorito, exibe o coração preenchido
+                                                : heartEmpty // Se não é favorito, exibe o coração vazio
+                                        }
+                                        alt="Favorite Icon"
+                                        className="favorite-icon"
+                                        onClick={() =>
+                                            toggleFavorite(character.id)
+                                        }
+                                        style={{ cursor: "pointer" }}
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
